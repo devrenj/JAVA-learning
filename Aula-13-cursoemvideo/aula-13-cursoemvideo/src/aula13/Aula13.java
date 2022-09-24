ca8ba367dd51ae12738289d5ad823e467b4ccda8
devrenj <110774837+devrenj@users.noreply.github.com>
2022-09-23 22:00:44 -0300

    Switch case tryout
    Scanner return string but some (polymorfism) overloaded methods receive integer as input

--- Aula-13-cursoemvideo/aula-13-cursoemvideo/src/aula13/Aula13.java
+++ Aula-13-cursoemvideo/aula-13-cursoemvideo/src/aula13/Aula13.java
@@ -21,7 +21,43 @@ public class Aula13 {
         c.reagir("oi toto");
         c.reagir("Cachorro");
         l.reagir("oi toto");    //Foi apenas uma piadinha :D
+        /* TENTATIVA DE IMPLEMENTAÇÃO DE UM LAÇO DO-WHILE
+        NÃO FUNCIONOU PORQUE O SWITCH CASE RETORNA UMA STRING
+        (MESMO QUE HAJA DUAS VARIÁVEIS SCANNER DE CADA TIPO)
+        POIS NA FUNÇÃO REAGIR(); TEMOS TAMBÉM ENTRADAS DO TIPO INT
+        --REALIZADO PARA FINS DIDÁTICOS, A FUNÇÃO REAGIR(STRING FRASE);
+        ESTÁ FUNCIONANDO PERFEITAMENTE. 
+        String input = "";
+        int inputTipo = 0;
+        do {
+            System.out.println("Com quem quer interagir?");
+            System.out.println("[1] Cachorro");
+            System.out.println("[2] Lobo");
+            inputTipo = scint.nextInt();
+        switch(inputTipo){
+            case 1:
+            System.out.println("Dê um comando para o cachorro:");        
+            input = sc.nextLine();
+            c.reagir(input);
+            trace();
+            // input = "";
+            // inputTipo = 0;
+            break;
 
+            case 2:
+            System.out.println("Dê um comando para o lobo:");        
+            input = sc.nextLine();
+            l.reagir(input);
+            trace();
+            // input = "";
+            // inputTipo = 0;
+            break;
+        }
+    } while (!"parar".equals(input));
+    
+    sc.close();
+    System.exit(1);
+    */
 }
     //Acho muito útil para testes de console
     public static void trace() {
